d0ce71510430ffd18d859b239b3b52f958e149fa
Patrick Wachsmuth <1026771+ebcFlagman@users.noreply.github.com>
2026-05-29 08:44:27 +0200

    #5 Update `CLAIM_PRESETS` with new set of values for streamlined configurations

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,4 +1,4 @@
-export const CLAIM_PRESETS = [20, 40, 50, 60, 70, 80, 90, 100, 120, 140, 150, 170, 200] as const;
+export const CLAIM_PRESETS = [20, 50, 100, 150, 200, 250, 300] as const;
 export const MULTIPLIERS = [1, 2, 3, 4, 5, 6] as const;
 export const MATCH_SCORE = 257;
 export const MAX_WRITE_SCORE = 157;
